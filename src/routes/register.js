e3a3b64ba415be056b1a0de6e10f26d5dc6b2ffd
Harshit <harshitkumar2468@gmail.com>
2021-07-05 14:23:09 +0530

    added authentication for registering a new user and password is auto generated for a new user

--- src/routes/register.js
+++ src/routes/register.js
@@ -2,6 +2,6 @@ const userController = require("../controllers/User.js");
 const router = require("express").Router();
 const authenticateToken = require("../middleware/authenticateToken");
 
-router.post(``, userController.create);
+router.post(``, authenticateToken, userController.create);
 
 module.exports = router;
