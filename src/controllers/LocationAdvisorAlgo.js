98f33b81b01ff0cd1eb6b602238141d19793ffcb
Harshit <harshitkumar2468@gmail.com>
2021-08-12 17:51:59 +0530

    changed location advisor algo

--- src/controllers/LocationAdvisorAlgo.js
+++ src/controllers/LocationAdvisorAlgo.js
@@ -10,12 +10,13 @@ const topMarketAreasInACity = async (city) => {
             $group: {
                 _id: "$address.freeformAddress",
                 shopCounts: { $sum: 1 },
+                // convert coordinates to only 5 decimal places
                 avgLat: { $avg: "$position.lat" },
                 avgLon: { $avg: "$position.lon" },
             },
         },
         {
-            $match: { shopCounts: { $gte: 10 } },
+            $match: { shopCounts: { $gte: 1 } },
         },
         {
             $sort: { shopCounts: -1 },
@@ -53,175 +54,136 @@ const totalEnablersAndDisablers = (scores) => {
 module.exports.newLocationAdvisor = async (req, res) => {
     try {
         let cityName = req.query.cityName;
-        let schema = req.body.schema; // [ { subcategory, radius, selected, disabler}];
+        let schema = req.body.schema; // [ { subcategory, radius, selected, disabler, index, weigt}];
         let schemaName = req.body.schemaName;
 
         console.log(`City Name is ${cityName}`);
-        console.log(`Schema is`, schema);
+        // console.log(`Schema is`, schema);
         // for (let sch of schema) {
         //     console.log(sch.subcategories);
         // }
         console.log(`Schema Name is ${schemaName}`);
 
-        let schemaCategories = [];
-        let begPromise = new Promise((resolve, reject) => {
-            schema.forEach((category, index, array) => {
-                category.subcategories.forEach((subcategory) => {
-                    if (subcategory.selected)
-                        schemaCategories.push(subcategory);
+        // Returns an array of categories which has been selected by the user
+        const setSchemaCategories = () => {
+            let begPromise = new Promise((resolve, reject) => {
+                let schemaCategories = [];
+                schema.forEach((category, index, array) => {
+                    category.subcategories.forEach((subcategory) => {
+                        if (subcategory.selected)
+                            schemaCategories.push(subcategory);
+                    });
+                    if (index === array.length - 1) {
+                        resolve(schemaCategories);
+                    }
                 });
-                if (index === array.length - 1) {
-                    resolve();
-                }
             });
+            return begPromise;
+        };
+
+        const schemaCategories = await setSchemaCategories();
+        console.log(`Schema Categories are `, schemaCategories);
+
+        if (schemaCategories.length === 0) {
+            return res.json({
+                error: true,
+                message:
+                    "Please select at least one subcategory to get suggestions",
+            });
+        }
+
+        let topMarketAreas = await topMarketAreasInACity(cityName);
+        console.log(`Top market areas are`, topMarketAreas);
+        console.log(`Top market areas size is ${topMarketAreas.length}`);
+
+        const marketAreasResult = [];
+
+        for (const marketArea of topMarketAreas) {
+            // const marketArea = topMarketAreas[0];
+            let { avgLat, avgLon } = marketArea;
+            let scoreArray = [];
+            let scores = [];
+
+            for (const category of schemaCategories) {
+                let { radius, subcategory, disabler, weight } = category;
+                let { lat, lon } = metresToLatLong(radius);
+                let score = await Establishment.countDocuments({
+                    "position.lat": {
+                        $gte: avgLat - lat,
+                        $lte: avgLat + lat,
+                    },
+                    "position.lon": {
+                        $gte: avgLon - lon,
+                        $lte: avgLon + lon,
+                    },
+                    "poi.categories": {
+                        $in: [subcategory],
+                    },
+                });
+                score = (disabler ? -1 * score : score) * (weight || 1);
+                console.log(`For category ${subcategory}, score is $ ${score}`);
+
+                scoreArray.push({
+                    ...category,
+                    score,
+                    weight: weight || 1,
+                });
+                scores.push(score);
+            }
+
+            console.log(`Score array is `, scoreArray);
+            console.log(`Scores are `, scores);
+
+            let areaScore = sumOfElements(scores);
+            const enablersPlusDisablers = totalEnablersAndDisablers(scores);
+            if (enablersPlusDisablers != 0) {
+                areaScore = (areaScore / enablersPlusDisablers) * 10;
+            }
+            console.log(`${marketArea._id} score is ${areaScore}`);
+
+            // adding this to the final result
+            marketAreasResult.push({ ...marketArea, scoreArray, areaScore });
+        }
+
+        marketAreasResult.sort(
+            (a, b) => parseFloat(b.areaScore) - parseFloat(a.areaScore)
+        );
+        console.log(`Market Areas result is : `, marketAreasResult);
+
+        // keeping only the top 15 market areas
+        marketAreasResult.slice(0, 15);
+
+        console.log(`Sorted Market Areas result is : `, marketAreasResult);
+
+        if (marketAreasResult[0].areaScore === 0) {
+            return res.json({
+                error: true,
+                // No good market areas
+                message:
+                    "Couldn't arrive at any suggestions! Please select more factors/categories to get a result.",
+            });
+        }
+
+        // if (schemaName) {
+        //     let locationAdvisorSchema = new LocationAdvisorSchema({
+        //         schemaName: schemaName,
+        //         // idealForBusiness: { type: String, default: "" },
+
+        //         subcategories: schema,
+        //     });
+        //     await locationAdvisorSchema.save();
+        // }
+
+        return res.json({
+            error: false,
+            marketAreasResult,
+            schemaMessage: schemaName ? `Schema saved as ${schemaName}` : null,
         });
 
-        begPromise
-            .then(async () => {
-                // if no subcategories selected, discard
-                if (schemaCategories.length === 0) {
-                    return res.json({
-                        error: true,
-                        message:
-                            "Please select at least one subcategory to get suggestions",
-                    });
-                }
-
-                console.log(schemaCategories);
-                let topMarketAreas = await topMarketAreasInACity(cityName);
-                // console.log(`Top market areas are`, topMarketAreas);
-                // console.log(
-                //     `Top market areas size is ${topMarketAreas.length}`
-                // );
-
-                async.parallel(
-                    // iterate each marketArea
-                    topMarketAreas.map((marketArea) => {
-                        return function (marketAreaCb) {
-                            let { avgLat, avgLon } = marketArea;
-                            let scoreArray = [];
-                            let scores = [];
-                            let prom = new Promise((resolve, reject) => {
-                                schemaCategories.forEach(
-                                    async (category, index, categories) => {
-                                        // calculate category score for this market
-                                        let {
-                                            selected,
-                                            radius,
-                                            subcategory,
-                                            disabler,
-                                            weight,
-                                        } = category;
-                                        if (selected) {
-                                            let { lat, lon } =
-                                                metresToLatLong(radius);
-                                            let score =
-                                                await Establishment.count({
-                                                    "position.lat": {
-                                                        $gte: avgLat - lat,
-                                                        $lte: avgLat + lat,
-                                                    },
-                                                    "position.lon": {
-                                                        $gte: avgLon - lon,
-                                                        $lte: avgLon + lon,
-                                                    },
-                                                    "poi.categories": {
-                                                        $in: [subcategory],
-                                                    },
-                                                });
-                                            score =
-                                                (disabler
-                                                    ? -1 * score
-                                                    : score) * (weight || 1);
-                                            scoreArray.push({
-                                                ...category,
-                                                score,
-                                                weight: weight || 1,
-                                            });
-                                            scores.push(score);
-                                        } else {
-                                            // changed from original
-                                            scores.push(0);
-                                            scoreArray.push({
-                                                ...category,
-                                                score: 0,
-                                                weight: weight || 1,
-                                            });
-                                        }
-                                        if (index === categories.length - 1) {
-                                            resolve();
-                                        }
-                                    }
-                                );
-                            });
-                            prom.then(() => {
-                                areaScore = sumOfElements(scores);
-                                const enablersPlusDisablers =
-                                    totalEnablersAndDisablers(scores);
-                                areaScore =
-                                    (areaScore / enablersPlusDisablers) * 10;
-                                console.log(
-                                    `${marketArea._id} score is ${areaScore}`
-                                );
-                                marketAreaCb(null, {
-                                    ...marketArea,
-                                    scoreArray,
-                                    areaScore,
-                                });
-                            });
-                        };
-                    }),
-                    async function (marketAreasErr, marketAreasResult) {
-                        /// final logic --- marketAreasResult = result with object having area address, score and category, radius etc.
-                        if (!marketAreasErr) {
-                            marketAreasResult.sort(
-                                (a, b) =>
-                                    parseFloat(b.areaScore) -
-                                    parseFloat(a.areaScore)
-                            );
-                            marketAreasResult = marketAreasResult.slice(0, 15);
-                            if (marketAreasResult[0].areaScore === 0) {
-                                return res.json({
-                                    error: true,
-                                    message:
-                                        "Couldn't arrive at any suggestions! Please select more factors/categories to get a result.",
-                                });
-                            }
-                            // if (schemaName) {
-                            //     let locationAdvisorSchema =
-                            //         new LocationAdvisorSchema({
-                            //             schemaName: schemaName,
-                            //             // idealForBusiness: { type: String, default: "" },
-
-                            //             subcategories: schema,
-                            //         });
-                            //     await locationAdvisorSchema.save();
-                            // }
-
-                            console.log(
-                                `Market area result is ${marketAreasResult}`
-                            );
-                            return res.json({
-                                error: false,
-                                marketAreasResult,
-                                schemaMessage: schemaName
-                                    ? `Schema saved as ${schemaName}`
-                                    : null,
-                            });
-                        } else {
-                            return res.json({
-                                error: true,
-                                errorMessage: marketAreasErr,
-                            });
-                        }
-                    }
-                );
-            })
-            .catch((err) => {
-                return res.json({ error: true, errorMessage: err });
-            });
-
-        // return res.json({ error: true, errorMessage: `Not an actual error` });
+        return res.json({
+            error: true,
+            errorMessage: `Not an actual error`,
+        });
     } catch (err) {
         console.log(err);
         return res.json({ error: true, errorMessage: err });
