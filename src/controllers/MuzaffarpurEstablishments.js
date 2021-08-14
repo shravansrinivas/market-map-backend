9faedd2f02c8684d8a406ee8f41121e18ed601fe
Harshit <harshitkumar2468@gmail.com>
2021-08-14 11:59:37 +0530

    optimized performance by using mongodb aggregation

--- src/controllers/MuzaffarpurEstablishments.js
+++ src/controllers/MuzaffarpurEstablishments.js
@@ -2,23 +2,47 @@ const MuzaffarpurEstablishment = require("../models/MuzaffarpurEstablishments");
 
 module.exports = {
     getMuzaffarpurEstablishments: async (req, res) => {
-        await MuzaffarpurEstablishment.find({})
-            .then((establishments) => {
-                const resultArray = [];
-                for (const est of establishments) {
-                    const { poi, address, position } = est;
-                    const { name, categories } = poi;
-                    resultArray.push({
-                        poi: { name, categories },
-                        address,
-                        position,
-                    });
-                }
-                res.send(resultArray);
-            })
-            .catch((err) => {
-                console.log(err);
-                res.send(err);
-            });
+        // await MuzaffarpurEstablishment.find({})
+        //     .then((establishments) => {
+        //         const resultArray = [];
+        //         for (const est of establishments) {
+        //             const { poi, address, position } = est;
+        //             const { name, categories } = poi;
+        //             resultArray.push({
+        //                 poi: { name, categories },
+        //                 address,
+        //                 position,
+        //             });
+        //         }
+        //         res.send(resultArray);
+        //     })
+        //     .catch((err) => {
+        //         console.log(err);
+        //         res.send(err);
+        //     });
+
+        try {
+            let establishments = await MuzaffarpurEstablishment.aggregate([
+                {
+                    $group: {
+                        _id: "$id",
+                        poi: { $first: "$poi" },
+                        address: { $first: "$address" },
+                        position: { $first: "$position" },
+                    },
+                },
+                {
+                    $project: {
+                        "poi.categorySet": 0,
+                        "poi.classifications": 0,
+                        _id: 0,
+                    },
+                },
+            ]);
+
+            res.json({ error: false, establishments });
+        } catch (err) {
+            res.json({ error: true, error: err });
+        }
     },
 };
